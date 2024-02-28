c036fde27fa3a6e9e0164f42fd287e7c898bc0c6
Robin Kareem <robin.r.kareem@gmail.com>
2024-02-28 22:13:06 +0100

    signin completed

---
+++ WebApp2/Models/SignInModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp2.Models;
+
+public class SignInModel
+{
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 0)]
+    [DataType(DataType.EmailAddress)]
+
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 1)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+
+    [Display(Name = "Remember me", Order = 2)]
+    public bool RememberMe { get; set; }
+}
